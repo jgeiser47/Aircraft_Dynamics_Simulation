c8517f1aa3ee58b1dd34bfb4f7be68bf4ff70f1f
Joshua Geiser <joshuageiser@Joshuas-MacBook-Air.local>
2018-09-27 11:57:30 -0400

    Got rid of hashbang for all nonexecutable files

--- sim_math.py
+++ sim_math.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 ####################################################################################################
 # Description:		Run simulation by executing Driver.py script
 # Author(s):		Joshua Geiser
