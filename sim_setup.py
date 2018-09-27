454ed8d28b6b953f33f4bde4fbe06221b893f84f
Joshua Geiser <joshuageiser@Joshuas-MacBook-Air.local>
2018-09-27 10:48:14 -0400

    Added progress bar

--- sim_setup.py
+++ sim_setup.py
@@ -43,7 +43,34 @@ def Run_Sim(SIM, plane1):
 		# Log output
 		log_data.Log_Output(TIME, plane1)
 
+		# Display percentage completion to console
+		printProgressBar(TIME, SIM.END_TIME, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 		# Increment time
 		TIME += SIM.DELTA_T
 
+	print()
+	print("\033[92m" + "Simulation Complete - Data output to post_processing/output.csv" + "\033[0m")
 
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+	"""
+	Call in a loop to create terminal progress bar
+	@params:
+	    iteration   - Required  : current iteration (Int)
+	    total       - Required  : total iterations (Int)
+	    prefix      - Optional  : prefix string (Str)
+	    suffix      - Optional  : suffix string (Str)
+	    decimals    - Optional  : positive number of decimals in percent complete (Int)
+	    length      - Optional  : character length of bar (Int)
+	    fill        - Optional  : bar fill character (Str)
+	"""
+	percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+	filledLength = int(length * iteration // total)
+	bar = fill * filledLength + '-' * (length - filledLength)
+	print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+
+	# Print New Line on Complete
+	if iteration == total: 
+		print()
